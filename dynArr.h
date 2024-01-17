364c055b3ef3a746f452eb203f4c272e1eac74d5
AngelJC03 <113469032+AngelJC03@users.noreply.github.com>
2024-01-17 02:04:09 -0500

    uploaded existing data structures and added BST

---
+++ dynArr.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <iostream>
+#include <array>
+#include <random>
+
+using namespace std;
+
+class DynArr {
+
+    private:
+
+        int *array;
+
+        int Size;
+
+        int Capacity;
+
+        void grow();
+
+        void shrink();
+
+        int findIndex(int n);
+
+        int partition(int *array, int start, int end);
+
+        void quickSort(int *array, int start, int end);
+
+    public:
+
+        DynArr();
+
+        DynArr(int capacity);
+
+        ~DynArr();
+
+        int size();
+
+        int capacity();
+
+        void push_front(int n);
+
+        void push_back(int n);
+
+        void removeIndex(int index);
+
+        void removeNum(int n);
+
+        int search(int n);
+
+        void print();
+
+        int sum();
+
+        void sort();
+
+};
